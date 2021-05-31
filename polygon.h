a1e9a2fc130da10e8fd762bca5fecb89a8a0f3bd
guart58 <80782747+guart58@users.noreply.github.com>
2021-05-31 22:59:32 +0700

    Create polygon.h

---
+++ polygon.h
@@ -0,0 +1,27 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+#include <string.h>
+#include <string>
+#include <time.h>
+
+class polygon {
+	string name;
+	int side;
+	double len;
+
+public:
+	void In();
+	void Out();
+	double Mod();
+
+	polygon() :name("dsa"), side(rand() % 5 + 3), len((double)(rand()) / RAND_MAX * 7) {}
+	~polygon() {}
+
+	friend ifstream& operator>>(ifstream& stream, polygon& V);
+//	friend ifstream& operator>>(ifstream& fstream, polygon& V);
+	friend ofstream& operator<<(ofstream& stream, polygon& V);
+//	friend ofstream& operator<<(ofstream& fstream, polygon& V);
+};
+
+#endif 
+
